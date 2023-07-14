7dc2d8f09ef7f353b7e2d1d972395f164d9d124f
Mike Kelly <mike.kelly@genesys.com>
2023-07-14 16:37:43 +0100

    select component implemented

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,42 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const OverriddenSelect = styled.select`
+    appearance: none;
+    color: ${COLORS.gray700};
+    background-color: ${COLORS.transparentGray15};
+    font-size: 1rem;
+    border-radius: 8px;
+    border: none;
+    padding: 12px 28px 12px 16px;
+`;
+
+const ChevronDown = styled(Icon)`
+    position: absolute;
+    top: 16px;
+    right: 28px;
+    pointer-events: none;
+`;
+
+const CustomSelect = styled.div`
+    position: relative;
+    width: fit-content;
+    padding-right: 12px;
+`;
+
+
+//const ChevronDown = styled(Icon('chevron-down', 6))``;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <CustomSelect>
+        <OverriddenSelect value={value} onChange={onChange}>
+          {children}
+        </OverriddenSelect>
+        <ChevronDown id="chevron-down" size={12}></ChevronDown>
+    </CustomSelect>
   );
 };
 
